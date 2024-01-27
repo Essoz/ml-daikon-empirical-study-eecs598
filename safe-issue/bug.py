f7f9375447c994eaa5d8cd1e455dfda600b3e6d2
ziming-zh <zimingzh@umich.edu>
2024-01-26 23:19:57 -0500

    Update model and results files 2 active layer left

--- safe-issue/bug.py
+++ safe-issue/bug.py
@@ -98,7 +98,8 @@ model_transfer.to(device)
 for name,param in model_transfer.module.named_parameters():
     total_nbn_layers = len([name for name, _ in model_transfer.named_parameters() if "bn" not in name])
     
-    tunable_nbn_layers = total_nbn_layers // 6
+    # tunable_nbn_layers = total_nbn_layers // 6
+    tunable_nbn_layers = 5
     tunable_nbn_ind = total_nbn_layers - tunable_nbn_layers
 
     count = 0
