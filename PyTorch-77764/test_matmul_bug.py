453e1b91428eaee628bf4074463c4434ec31b2b0
Yuxuan <yuxuanj9@illinois.edu>
2024-02-03 11:29:05 -0500

    minor edits

--- PyTorch-77764/test_matmul_bug.py
+++ PyTorch-77764/test_matmul_bug.py
@@ -9,7 +9,7 @@ def test_matmul_bug():
     ones_mps = torch.ones(1, 32769, device=torch.device("mps"))
     result_mps = zeros_mps @ ones_mps
 
-    if not torch.allclose(result_cpu, result_mps):
+    if not torch.allclose(result_cpu, result_mps.to('cpu')):
         raise AssertionError("Results are different between CPU and MPS devices")
 
 test_matmul_bug()
